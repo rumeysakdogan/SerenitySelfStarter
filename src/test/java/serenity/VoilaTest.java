c8ab734cd73180d3459afb24699a84b36da135b5
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-12-27 12:31:44 -0700

    Added a test that contains multiple smaller steps that can be reusable

---
+++ src/test/java/serenity/VoilaTest.java
@@ -0,0 +1,20 @@
+package serenity;
+
+import net.serenitybdd.junit5.SerenityTest;
+import org.junit.jupiter.api.Test;
+
+@SerenityTest
+public class VoilaTest {
+
+    @Test
+    public void testVoila(){
+
+        // B20USER preparedSomething
+
+        // B20USER takeAnAction
+
+        // B20USER expectSomeResult
+
+
+    }
+}
