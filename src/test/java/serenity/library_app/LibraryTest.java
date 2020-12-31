f63d6f47686d13c798badb2cb0bb66290db465cd
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-12-30 17:05:02 -0700

    LibraryTest has been updated to work with latest change

--- src/test/java/serenity/library_app/LibraryTest.java
+++ src/test/java/serenity/library_app/LibraryTest.java
@@ -6,33 +6,20 @@ import net.serenitybdd.junit5.SerenityTest;
 import net.serenitybdd.rest.Ensure;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Steps;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import serenity.utility.ConfigReader;
 
 import static net.serenitybdd.rest.SerenityRest.given;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
 import static org.hamcrest.Matchers.*;
 
 @SerenityTest
 public class LibraryTest {
-
-    @Steps
-    ConfigReader conf;
-
-//    @BeforeAll
-//    public static void setUp(){
-//        RestAssured.baseURI = "http://library1.cybertekschool.com";
-//        RestAssured.basePath = "/rest/v1" ;
-//    }
-
-    @BeforeEach
-    public  void setUpEach(){
-        RestAssured.baseURI = conf.getProperty("base.url");
-        RestAssured.basePath = conf.getProperty("base.path"); ;
+    @BeforeAll
+    public static void setUp(){
+        RestAssured.baseURI  = ConfigReader.getProperty("base.url") ;
+        RestAssured.basePath = ConfigReader.getProperty("base.path") ;
     }
-
     @AfterAll
     public static void tearDown(){
         RestAssured.reset();
@@ -40,27 +27,62 @@ public class LibraryTest {
     }
 
     @Test
+    public void testingReadingConfigFile(){
+
+        System.out.println("conf.getProperty(\"base.url\") = "
+                + ConfigReader.getProperty("base.url"));
+        System.out.println("conf.getProperty(\"librarian.username\") = "
+                + ConfigReader.getProperty("librarian.username"));
+
+    }
+
+    @Test
     public void testLogin(){
         given()
                 .log().all()
                 .contentType( ContentType.URLENC  )
-                .formParam("email", conf.getProperty("librarian.username"))
-                .formParam("password", conf.getProperty("librarian.password")).
+                .formParam("email", ConfigReader.getProperty("librarian.username"))
+                .formParam("password",ConfigReader.getProperty("librarian.password")).
                 when()
-                .post("/login") ;
+                .post("/login").prettyPeek().
+                then()
+                .statusCode(200)
+        ;
 
-        Ensure.that("Getting successful result",
-                vRes -> vRes.statusCode(is(200)));
+        Ensure.that("Getting Successful Result",
+                vRes -> vRes.statusCode(200)
+        ).andThat("token is not null",
+                vRes -> vRes.body("token", is(notNullValue()))
+        ) ;
+
     }
 
+    @DisplayName("Get Dashboard Stats Test")
     @Test
-    public void testingReadingConfigFile(){
+    public void zDashboardStatTest() {
+        // better way is creating utility to get the token
+        // and call it under before all
+        // we will just manipulate the order here by adding z in front of the test
+        String token = lastResponse().jsonPath().getString("token");
+        given()
+                .header("x-library-token", token).
+                when()
+                .get("/dashboard_stats").prettyPeek();
 
-        System.out.println("conf.getProperty(\"base.url\") = "
-                + conf.getProperty("base.url"));
+        Ensure.that("Successful result was received",
+                vRes -> vRes.statusCode(is(200)))
+                .andThat("All 3 fields are not null",
+                        vRes -> vRes.body("book_count", notNullValue())
+                                .body("borrowed_books", notNullValue())
+                                .body("users", notNullValue())
+                );
+
+        //TODO : Get the expected data from database
+        //TODO : Ensure that the results match
+
 
         // mvn clean verify -Denvironment=library2
+
+
     }
-
-
 }
