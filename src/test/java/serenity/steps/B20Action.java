950042fc7af8f31e4feb7d9522fdb296b8f45aa7
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-12-30 17:06:51 -0700

    added actor to have better Displayname in our test

--- src/test/java/serenity/steps/B20Action.java
+++ src/test/java/serenity/steps/B20Action.java
@@ -4,17 +4,21 @@ import net.thucydides.core.annotations.Step;
 
 public class B20Action {
 
-    @Step
+    String actor ;
+
+    // Reports are getting step names from method name
+    // unless we did not specify @Step("...") inside step annotation
+    @Step("This is where #actor prepare all stuff before taking action")
     public void preparedSomething(){
         System.out.println("preparing cool stuff");
     }
 
-    @Step
+    @Step("#actor Taking some awesome action here")
     public void takeAnAction(){
         System.out.println("taking some action");
     }
 
-    @Step
+    @Step("#actor Eventually expecting a tremendous result")
     public void expectSomeResult(){
         System.out.println("expecting some result");
     }
